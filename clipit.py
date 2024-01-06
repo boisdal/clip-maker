9c128f0d3817a61fe8a5173baa491bc5a38dc5d6
Vincent Boisdon <vfj.bois@gmail.com>
2024-01-06 17:01:03 +0100

    Ajout du scenarion json

--- clipit.py
+++ clipit.py
@@ -1,5 +1,6 @@
 from moviepy.editor import *
 from PIL import Image, ImageDraw, ImageFont
+import json
 
 # Couleurs
 bleu = "#1423DC"
@@ -13,6 +14,15 @@ h_grille = 180
 ph = 36
 ml = 90
 mb = 22
+
+# Classes
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    def __getattr__(*args):
+         val = dict.get(*args)
+         return dotdict(val) if type(val) is dict else val
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
 
 # Fonctions
 def ecrire_ligne_titre(draw, index, texte):
@@ -33,45 +43,49 @@ def importer_clip(nom):
     clip = VideoFileClip(nom, has_mask=True)
     original_size = clip.size
     if original_size != [1080, 1080]:
-        print(f"resizing clip {nom}")
         clip = VideoClip.crop(clip, width=1080, height=1080, x_center=original_size[0]/2, y_center=original_size[1]/2)
     return clip
 
+with open("scenario.json", encoding='utf-8') as f:
 # Début exécution
-print("starting")
-liste_clips = []
-duree_actuelle = 0
+    scenario = dotdict(json.load(f))
+    liste_clips = []
+    duree_actuelle = 0
 
 # Intro
-intro = importer_clip("sources/videos/1x1/intro 3s.mov").set_layer(2)
-liste_clips.append(intro)
-duree_actuelle += 2
+    intro = importer_clip("sources/videos/1x1/intro 3s.mov").set_layer(2)
+    liste_clips.append(intro)
+    duree_actuelle += 2
 
 # Titre
-creer_image_titre('titre', ["Le compteur linky,", "un outil pour", "la transition écologique"])
-titre = ImageClip("intermediaires/titre.png", duration=5).set_start(duree_actuelle)
-liste_clips.append(titre)
-fil = ImageClip("sources/img/Fil_blanc.jpg", duration=titre.duration).set_start(duree_actuelle).set_layer(1)
-fil = fil.set_position(lambda t: (pow(100, t-4) * 300, 2*h_grille+2*h_ligne+2*mb+pb+60))
-liste_clips.append(fil)
-carre = ImageClip("sources/img/blue.jpg", duration=titre.duration).set_start(duree_actuelle).set_layer(1)
-carre = carre.set_position(lambda t: (pow(100, t-4.2) * 300 - 1080 if (pow(100, t-4.2) * 300) < 1080 else 0, 0))
-liste_clips.append(carre)
-duree_actuelle += titre.duration
+    d_titre = scenario.titre.duree
+    creer_image_titre('titre', scenario.titre.texte)
+    titre = ImageClip("intermediaires/titre.png", duration=d_titre).set_start(duree_actuelle)
+    liste_clips.append(titre)
+    fil = ImageClip("sources/img/Fil_blanc.jpg", duration=titre.duration).set_start(duree_actuelle).set_layer(1)
+    fil = fil.set_position(lambda t: (pow(100, t-d_titre+1) * 300, 2*h_grille+2*h_ligne+2*mb+pb+60))
+    liste_clips.append(fil)
+    carre = ImageClip("sources/img/blue.jpg", duration=titre.duration).set_start(duree_actuelle).set_layer(1)
+    carre = carre.set_position(lambda t: (pow(100, t-d_titre+1.1) * 300 - 1080 if (pow(100, t-d_titre+1.1) * 300) < 1080 else 0, 0))
+    liste_clips.append(carre)
+    duree_actuelle += titre.duration
 
 # Corps du clip
-clip = importer_clip('test.mp4').set_layer(0).subclip(0,4).set_start(duree_actuelle)
-liste_clips.append(clip)
-duree_actuelle += clip.duration
+    for seg in scenario.liste_segments:
+        segment = dotdict(seg)
+        if segment.type == "clip":
+            clip = importer_clip(segment.source).set_layer(0).subclip(segment.debut,segment.fin).set_start(duree_actuelle)
+            liste_clips.append(clip)
+            duree_actuelle += clip.duration
 
 # Outro
-outro = importer_clip("sources/videos/1x1/outro 3s.mov").set_layer(1).set_start(duree_actuelle-0.5)
-liste_clips.append(outro)
+    outro = importer_clip("sources/videos/1x1/outro 3s.mov").set_layer(1).set_start(duree_actuelle-0.5)
+    liste_clips.append(outro)
 
 # Montage final
-video = CompositeVideoClip(liste_clips)
-video.write_videofile('output.mp4')
+    video = CompositeVideoClip(liste_clips)
+    video.write_videofile('output.mp4', logger=None)
 
 # Fermeture des clips
-for clip in liste_clips:
-    clip.close()
+    for clip in liste_clips:
+        clip.close()
